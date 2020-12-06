8367e38a644370cab03195b32ff0c746054f3869
Johan Cardell <Johan.Cardell@iths.se>
2020-12-06 21:56:13 +0100

    Code Cleanup

--- Program.cs
+++ Program.cs
@@ -10,22 +10,31 @@ namespace TollFeeCalculator
             Run(Environment.CurrentDirectory + "../../../../testData.txt");
         }
 
-        static public void Run(string filePath) {
-          
-            Console.Write(MessageBasedOnInPutFile(filePath));
+        static public void Run(string filePath)
+        {
+            Console.Write(MessageBasedOnFileContent(filePath));
         }
 
-        private static string MessageBasedOnInPutFile(string filePath)
+        private static string MessageBasedOnFileContent(string filePath)
         {
-            string message = string.Empty;
-            var loggedTollPassages = GetPassagesFromFile(filePath, ref message);
-            if (message.Length == 0)
+            var errorMessage = string.Empty;
+            var passages = GetPassagesFromFile(filePath, ref errorMessage);
+            string message;
+            if (Error(errorMessage))
             {
-                message = "The total fee for the inputfile is " + TotalFeeCost(loggedTollPassages);
+                message = errorMessage;
+            }
+            else
+            {
+                message = "The total fee for the inputfile is " + TotalFeeCost(passages);
             }
             return message;
         }
 
+        static bool Error(string message)
+        {
+            return message.Length > 0; 
+        }
 
         private static DateTime[] GetPassagesFromFile(string filePath, ref string errorMessage)
         {
@@ -50,128 +59,82 @@ namespace TollFeeCalculator
                 catch (Exception e)
                 {
                     errorMessage = e.Message;
+                    return null;
                 }
             }
             return loggedTollPassages;
         }
 
-
-        //private static DateTime[] GetPassagesFromFile(string filePath, ref string errorMessage)
-        //{
-        //    string indata = string.Empty;
-        //    try
-        //    {
-        //        indata = System.IO.File.ReadAllText(filePath);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        errorMessage = e.Message;
-        //    }
-        //    string[] dateStrings = indata.Split(", ");
-        //    DateTime[] loggedTollPassages = new DateTime[dateStrings.Length];
-        //    for (int i = 0; i < loggedTollPassages.Length; i++)
-        //    {
-        //        if (DateTime.TryParse(dateStrings[i], out DateTime result))
-        //        {
-        //            loggedTollPassages[i] = result;
-        //        }
-        //    }
-        //    return loggedTollPassages;
-        //}
-
-        //private static bool ReadFile(string filePath, out DateTime[] loggedTollPassages, ref string errorMessage)
-        //{
-        //    bool fileIsInCorrectFormat = true;
-        //    string indata = string.Empty;
-        //    try
-        //    {
-        //        indata = System.IO.File.ReadAllText(filePath);
-        //    }
-        //    catch (Exception e)
-        //    {
-        //        errorMessage = e.Message;
-        //    }
-        //    string[] dateStrings = indata.Split(", ");
-        //        loggedTollPassages = new DateTime[dateStrings.Length];
-        //        for (int i = 0; i < loggedTollPassages.Length; i++)
-        //        {
-        //            if (DateTime.TryParse(dateStrings[i], out DateTime result))
-        //            {
-        //                loggedTollPassages[i] = result;
-        //            }
-        //            else fileIsInCorrectFormat = false;
-        //        }
-        //    return fileIsInCorrectFormat;
-        //}
-
         static public int TotalFeeCost(DateTime[] loggedTollPassages) {
-            List<DateTime[]> passagesSortedByDays = SortPassagesByDay(loggedTollPassages);
-            int totalFee = 0;
+            var totalFee = 0;
             const int MAXFEE = 60;
             DateTime hourIntervalStart = loggedTollPassages[0]; //Starting interval
-            totalFee += TollFeePass(hourIntervalStart);
-            for (int i = 1; i < loggedTollPassages.Length - 1; i++)
+            var initialFeeToday = TollFeePass(hourIntervalStart);
+            var totalFeeToday = initialFeeToday;
+            for (int i = 1; i < loggedTollPassages.Length; i++)
             {
                 var currentPassage = loggedTollPassages[i];
                 var currentFee = TollFeePass(currentPassage);
-                var diffInMinutes = currentPassage.Subtract(hourIntervalStart).TotalMinutes;
-                if (diffInMinutes < 60)
+                if (SameDay(currentPassage, hourIntervalStart))
                 {
-                    totalFee += AddTheHighestFee(currentFee, TollFeePass(hourIntervalStart));
+                    if (WithinAnHour(currentPassage, hourIntervalStart))
+                    {
+                        totalFeeToday += OnlyUseHighestFee(currentFee, TollFeePass(hourIntervalStart));
+                    }
+                    else
+                    {
+                        totalFeeToday += currentFee;
+                        hourIntervalStart = currentPassage;
+                    }
                 }
                 else
                 {
-                    totalFee += currentFee;
+                    totalFee += Math.Min(totalFeeToday, MAXFEE);
+                    totalFeeToday = 0;
+                    initialFeeToday = TollFeePass(currentPassage);
+                    totalFeeToday += initialFeeToday;
                     hourIntervalStart = currentPassage;
                 }
             }
-            //foreach (var currentPassageTime in loggedTollPassages)
-            //{
-            //    //var diffInMinutes = currentPassageTime.Subtract(hourIntervalStart).TotalMinutes;
-            //    long diffInMinutes = (currentPassageTime - hourIntervalStart).Minutes;
-            //    if (diffInMinutes < 60) {
-            //        totalFee += AddTheHighestFee(TollFeePass(currentPassageTime),TollFeePass(hourIntervalStart));
-            //    }
-            //    else {
-            //        totalFee += TollFeePass(currentPassageTime);
-            //        hourIntervalStart = currentPassageTime;
-            //        //if (TollFeePass(currentPassageTime) < TollFeePass(previousPassageTime))
-            //    }
-            //}
-
-            return Math.Min(totalFee, MAXFEE);
+            return totalFee + totalFeeToday;
         }
 
-        private static List<DateTime[]> SortPassagesByDay(DateTime[] loggedTollPassages)
+        static bool SameDay(DateTime current, DateTime previous)
         {
-            List<DateTime[]> passsagesDividedByDate = new List<DateTime[]>();
-            passsagesDividedByDate.Add(loggedTollPassages.g)
-            loggedTollPassages.
+            return current.Date == previous.Date;
         }
 
-        static public int TollFeePass(DateTime d)
+        static bool WithinAnHour(DateTime current, DateTime previous)
         {
-            if (Free(d)) return 0;
-            int hour = d.Hour;
-            int minute = d.Minute;
-            if (hour == 6 && minute >= 0 && minute <= 29) return 8;
-            else if (hour == 6 && minute >= 30 && minute <= 59) return 13;
-            else if (hour == 7 && minute >= 0 && minute <= 59) return 18;
-            else if (hour == 8 && minute >= 0 && minute <= 29) return 13;
-            else if (hour == 8 && minute >= 30 && minute <= 59) return 8;
-            else if (hour >= 9 && hour <= 14) return 8;
-            else if (hour == 15 && minute >= 0 && minute <= 29) return 13;
-            else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) return 18;
-            else if (hour == 17 && minute >= 0 && minute <= 59) return 13;
-            else if (hour == 18 && minute >= 0 && minute <= 29) return 8;
-            else return 0;
+            return (current.Subtract(previous).TotalMinutes < 60);
         }
 
-        static public bool Free(DateTime date) {
-        return (int)date.DayOfWeek == 0 || (int)date.DayOfWeek == 6 || date.Month == 7;
+        static public int TollFeePass(DateTime passage)
+        {
+            int hour = passage.Hour;
+            int minute = passage.Minute;
+            int fee;
+            if (Free(passage)) fee = 0;
+            else if (hour == 6 && minute >= 0 && minute <= 29) fee = 8;
+            else if (hour == 6 && minute >= 30 && minute <= 59) fee = 13;
+            else if (hour == 7 && minute >= 0 && minute <= 59) fee = 18;
+            else if (hour == 8 && minute >= 0 && minute <= 29) fee = 13;
+            else if (hour == 8 && minute >= 30 && minute <= 59) fee = 8;
+            else if (hour >= 9 && hour <= 14) fee = 8;
+            else if (hour == 15 && minute >= 0 && minute <= 29) fee = 13;
+            else if (hour == 15 && minute >= 0 || hour == 16 && minute <= 59) fee = 18;
+            else if (hour == 17 && minute >= 0 && minute <= 59) fee = 13;
+            else if (hour == 18 && minute >= 0 && minute <= 29) fee = 8;
+            else fee = 0;
+            return fee;
         }
 
-        static int AddTheHighestFee(int currentFee, int previousFee)
+        static public bool Free(DateTime date)
+        {
+            return (int)date.DayOfWeek == 0 || (int)date.DayOfWeek == 6 || date.Month == 7;
+        }
+
+        private static int OnlyUseHighestFee(int currentFee, int previousFee)
         {
             int fee = 0;
             if(currentFee > previousFee)
